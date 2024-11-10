8163a5d0dfd7849e4cec1dbb4ec6993c23ceb1b9
hexnz <Renandefreitasc@Hotmail.com>
2024-11-10 19:26:03 -0300

    iniciando gerenciador de tarefas

---
+++ FullStackT2/Exercicios/testePessoais/GerenciadorTarefas/script.js
@@ -0,0 +1,12 @@
+const taskList=document.getElementById("taskList")
+const taskInput=document.getElementById("taskInput")
+const addTaskBtn=document.getElementById("addTaskBtn")
+const filters =document.querySelectorAll('filterBtn');
+
+addTaskBtn.addEventListener('click',() =>{
+
+const taskTex=taskInput.value.trim();
+if(taskTex)
+
+
+})
